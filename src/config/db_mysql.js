733305f91a058e287ce6415d36f1f9a87591004d
beesaiyvoud <laoseasybee@gmail.com>
2024-12-18 19:11:53 +0700

    add

--- src/config/db_mysql.js
+++ src/config/db_mysql.js
@@ -1,11 +1,14 @@
 import mysql from "mysql";
 
-const connected = mysql.createConnection({
+const connected = mysql.createPool({
   host: "mysql-189252-0.cloudclusters.net",
   port: "10034",
   user: "admin",
   password: "QZuCyO6Q",
   database: "db_restaurant",
+  waitForConnections: true, // Wait for connections when the pool is full
+  connectionLimit: 10, // Maximum number of connections in the pool
+  queueLimit: 0, 
 });
 // const connected = mysql.createConnection({
 //   host: "localhost",
@@ -13,7 +16,8 @@ const connected = mysql.createConnection({
 //   password: "",
 //   database: "db_restaurant",
 // })
-connected.connect((err) => {
+
+connected.getConnection((err) => {
   if (err) console.log(`Faild Connected Database` + err);
   console.log(`Connected Database`);
 });
